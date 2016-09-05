926afd6b8b5da2296a70f8904a556672414eb1c0
HoldOffHunger <holdoffhunger@gmail.com>
2016-09-05 12:01:54 -0400

    Update generator.js

--- generator.js
+++ generator.js
@@ -62,7 +62,122 @@
 			name: 'Delicious',
 			class: 'delicious',
 			url: 'https://delicious.com/save?v=5&provider={provider}&noui&jump=close&url={url}&title={title}',
-		}
+		},
+		{
+			name: 'Blogger',
+			class: 'blogger',
+			url: 'https://www.blogger.com/blog-this.g?u={url}&n={title}&t={desc}',
+		},
+		{
+			name: 'LiveJournal',
+			class: 'livejournal',
+			url: 'http://www.livejournal.com/update.bml?subject={title}&event={url}',
+		},
+		{
+			name: 'MySpace',
+			class: 'myspace',
+			url: 'https://myspace.com/post?u={url}&t={title}&c=desc',
+		},
+		{
+			name: 'Yahoo',
+			class: 'yahoo',
+			url: 'http://compose.mail.yahoo.com/?body={url}',
+		},
+		{
+			name: 'FriendFeed',
+			class: 'friendfeed',
+			url: 'http://friendfeed.com/?url={url}',
+		},
+		{
+			name: 'NewsVine',
+			class: 'newsvine',
+			url: 'http://www.newsvine.com/_tools/seed&save?u={url}',
+		},
+		{
+			name: 'EverNote',
+			class: 'evernote',
+			url: 'http://www.evernote.com/clip.action?url={url}',
+		},
+		{
+			name: 'GetPocket',
+			class: 'getpocket',
+			url: 'https://getpocket.com/save?url={url}',
+		},
+		{
+			name: 'FlipBoard',
+			class: 'flipboard',
+			url: 'https://share.flipboard.com/bookmarklet/popout?v=2&title={title}&url={url}',
+		},
+		{
+			name: 'InstaPaper',
+			class: 'instapaper',
+			url: 'http://www.instapaper.com/edit?url={url}&title={title}&description={desc}',
+		},
+		{
+			name: 'Line.me',
+			class: 'lineme',
+			url: 'http://line.me/R/msg/text/?{url}',
+		},
+		{
+			name: 'Skype',
+			class: 'skype',
+			url: 'https://web.skype.com/share?url={url}',
+		},
+		{
+			name: 'Viber',
+			class: 'viber',
+			url: 'viber://forward?text={url}',
+		},
+		{
+			name: 'WhatsApp',
+			class: 'whatsapp',
+			url: 'whatsapp://send?text={url}',
+		},
+		{
+			name: 'Telegram.me',
+			class: 'telegramme',
+			url: 'https://telegram.me/share/url?url={url}&text={title}',
+		},
+		{
+			name: 'VK',
+			class: 'vk',
+			url: 'http://oauth.vk.com/authorize?client_id=-1&redirect_uri={url}&display=widget&caption={title}',
+		},
+		{
+			name: 'OK.ru',
+			class: 'okru',
+			url: 'https://connect.ok.ru/dk?st.cmd=WidgetSharePreview&st.shareUrl={url}&title={title}',
+		},
+		{
+			name: 'Douban',
+			class: 'douban',
+			url: 'http://www.douban.com/recommend/?url={url}&title={title}',
+		},
+		{
+			name: 'Baidu',
+			class: 'baidu',
+			url: 'http://cang.baidu.com/do/add?it={title}&iu={url}',
+		},
+		{
+			name: 'QZone',
+			class: 'qzone',
+			url: 'http://sns.qzone.qq.com/cgi-bin/qzshare/cgi_qzshare_onekey?url={url}',
+		},
+		{
+			name: 'Xing',
+			class: 'xing',
+			url: 'https://www.xing.com/app/user?op=share&url={url}',
+		},
+		{
+			name: 'RenRen',
+			class: 'renren',
+			url: 'http://widget.renren.com/dialog/share?resourceUrl={url}&srcUrl={url}&title={title}',
+		},
+		{
+			name: 'Weibo',
+			class: 'weibo',
+			url: 'http://service.weibo.com/share/share.php?url={url}&appkey=&title={text}&pic=&ralateUid=',
+		},
 	];
 
 	SocialShare.generateSocialUrls = function(opt) {
